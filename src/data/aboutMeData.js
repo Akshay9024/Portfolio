5680215ff7b35c5b9ec28aedc42e042e17c67707
Akshay <mukkera9024@gmail.com>
2024-11-12 23:07:49 +0530

    first commit

---
+++ src/data/aboutMeData.js
@@ -0,0 +1,10 @@
+export const aboutMeData = [
+  {
+    id: 1,
+    bio: "My name is Akshay, and I am currently a second-year B.Tech student at IIT Mandi. I have a keen interest in web development and machine learning.",
+  },
+  {
+    id: 2,
+    bio: "I am writing to express my fervent interest in Web Development. I've been eager to work in this field for a while, and the opportunity to contribute using my skills in HTML, CSS, JavaScript, Bootstrap, React.js, Node.js, and MongoDB is incredibly exciting. With a strong foundation in web development principles and a passion for creating user-friendly, high-performing web experiences, I am eager to learn and grow alongside a talented team. I am confident that I can quickly become a valuable asset.",
+  },
+];
